00287aae1759e44762a86468ce77a1748076806a
Simon Walker <s.r.walker101@googlemail.com>
2022-12-16 09:25:55 +0000

    try to get icon file working

--- lint.sh
+++ lint.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 rm -rf dist
 npm run dist
-./dist/Notion-0.0.4.AppImage --appimage-extract
+./dist/Notion-0.0.5.AppImage --appimage-extract
 cat ./squashfs-root/notion.desktop
 desktop-file-validate ./squashfs-root/notion.desktop
